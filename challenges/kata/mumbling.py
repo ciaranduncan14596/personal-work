2dc1cd62e4eeebfa09c7fd5dc7b1ca54f71dd39c
Ciaran Duncan <ciaran.duncan@rakuten.com>
2021-08-13 08:12:56 +0100

    Add mumbling kata

---
+++ challenges/kata/mumbling.py
@@ -0,0 +1,20 @@
+'''
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+'''
+
+def accum(word):
+    wordToReturn = ''
+    for i in range(0, len(word)):
+        wordToReturn += word[i].upper() + word[i].lower()*i + '-'
+        
+    return wordToReturn[:-1]
+
+a = accum('RqaEzty')
+print(a)
