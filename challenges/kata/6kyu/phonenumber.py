a49a0d47d9cb6ceee06afaf79b52c3e27eaa7c9d
Ciaran Duncan <ciaran.duncan@googlemail.com>
2021-09-06 08:06:11 +0100

    6kyu phonenumber.py

---
+++ challenges/kata/6kyu/phonenumber.py
@@ -0,0 +1,18 @@
+'''
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+'''
+
+def create_phone_number(n):
+
+    s_s = n[0:3]
+    print(str(s_s))
+
+    s = '(' + str(''.join(str(e) for e in n[0:3])) + ') ' + ''.join(str(e) for e in n[3:6]) + '-'+''.join(str(e) for e in n[6:])
+    return s
+
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
